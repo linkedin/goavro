53d4b355a9f34ffd733a21788884d0488bfa2073
Karrick S. McDermott <kmcdermott@linkedin.com>
2015-09-15 13:19:14 -0400

    tests fix for issue #15; goavro.Reader and goavro.Writer tested to ensure can process null, deflate, and snappy codecs

--- ocf_writer_test.go
+++ ocf_writer_test.go
@@ -100,11 +100,11 @@ func TestWriteHeaderCustomSync(t *testing.T) {
 
 	// NOTE: because key value pair ordering is indeterminate,
 	// there are two valid possibilities for the encoded map:
-	option1 := []byte("Obj\x01\x04\x14avro.codec\x08null\x16avro.schema\x0a\x22int\x22\x00\x21\x0f\xc7\xbb\x81\x86\x39\xac\x48\xa4\xc6\xaf\xa2\xf1\x58\x1a\x00\x00")
-	option2 := []byte("Obj\x01\x04\x16avro.schema\x0a\x22int\x22\x14avro.codec\x08null\x00\x21\x0f\xc7\xbb\x81\x86\x39\xac\x48\xa4\xc6\xaf\xa2\xf1\x58\x1a\x00\x00")
+	option1 := []byte("Obj\x01\x04\x14avro.codec\x08null\x16avro.schema\x0a\x22int\x22\x00\x21\x0f\xc7\xbb\x81\x86\x39\xac\x48\xa4\xc6\xaf\xa2\xf1\x58\x1a")
+	option2 := []byte("Obj\x01\x04\x16avro.schema\x0a\x22int\x22\x14avro.codec\x08null\x00\x21\x0f\xc7\xbb\x81\x86\x39\xac\x48\xa4\xc6\xaf\xa2\xf1\x58\x1a")
 
 	actual := bb.Bytes()
-	if (bytes.Compare(actual, option1) != 0) && (bytes.Compare(actual, option2) != 0) {
+	if !bytes.Equal(actual, option1) && !bytes.Equal(actual, option2) {
 		t.Errorf("Actual: %#v; Expected: %#v", actual, option1)
 	}
 }
@@ -126,16 +126,16 @@ func TestWriteWithNullCodec(t *testing.T) {
 
 	// NOTE: because key value pair ordering is indeterminate,
 	// there are two valid possibilities for the encoded map:
-	option1 := []byte("Obj\x01\x04\x14avro.codec\x08null\x16avro.schema\x0a\x22int\x22\x00" + string(defaultSync) + "\x08\x0a\x1a\x54\x6c\xc6\x01" + string(defaultSync) + "\x00\x00")
-	option2 := []byte("Obj\x01\x04\x16avro.schema\x0a\x22int\x22\x14avro.codec\x08null\x00" + string(defaultSync) + "\x08\x0a\x1a\x54\x6c\xc6\x01" + string(defaultSync) + "\x00\x00")
+	option1 := []byte("Obj\x01\x04\x14avro.codec\x08null\x16avro.schema\x0a\x22int\x22\x00" + string(defaultSync) + "\x08\x0a\x1a\x54\x6c\xc6\x01" + string(defaultSync))
+	option2 := []byte("Obj\x01\x04\x16avro.schema\x0a\x22int\x22\x14avro.codec\x08null\x00" + string(defaultSync) + "\x08\x0a\x1a\x54\x6c\xc6\x01" + string(defaultSync))
 
 	actual := bb.Bytes()
-	if (bytes.Compare(actual, option1) != 0) && (bytes.Compare(actual, option2) != 0) {
+	if !bytes.Equal(actual, option1) && !bytes.Equal(actual, option2) {
 		t.Errorf("Actual: %#v; Expected: %#v", actual, option1)
 	}
 }
 
-func _TestWriteWithDeflateCodec(t *testing.T) {
+func TestWriteWithDeflateCodec(t *testing.T) {
 	bb := new(bytes.Buffer)
 	func(w io.Writer) {
 		fw, err := NewWriter(
@@ -156,11 +156,43 @@ func _TestWriteWithDeflateCodec(t *testing.T) {
 
 	// NOTE: because key value pair ordering is indeterminate,
 	// there are two valid possibilities for the encoded map:
-	option1 := []byte("Obj\x01\x04\x14avro.codec\x08null\x16avro.schema\x0a\x22int\x22\x00" + string(defaultSync) + "\x08\x0a\x1a\x54\x6c\xc6\x01" + string(defaultSync) + "\x00\x00")
-	option2 := []byte("Obj\x01\x04\x16avro.schema\x0a\x22int\x22\x14avro.codec\x08null\x00" + string(defaultSync) + "\x08\x0a\x1a\x54\x6c\xc6\x01" + string(defaultSync) + "\x00\x00")
+	compressed := "\x04\x10\x92\x0a\x01\x04\x00\x00\xff\xff\x21\x0f\xc7\xbb\x81\x86\x39\xac\x48\xa4\xc6\xaf\xa2\xf1\x58\x1a\x04\x12\xca\x39\xc6\x08\x08\x00\x00\xff\xff"
+	option1 := []byte("Obj\x01\x04\x14avro.codec\x0edeflate\x16avro.schema\x0a\x22int\x22\x00" + string(defaultSync) + compressed + string(defaultSync))
+	option2 := []byte("Obj\x01\x04\x16avro.schema\x0a\x22int\x22\x14avro.codec\x0edeflate\x00" + string(defaultSync) + compressed + string(defaultSync))
 
 	actual := bb.Bytes()
-	if (bytes.Compare(actual, option1) != 0) && (bytes.Compare(actual, option2) != 0) {
+	if !bytes.Equal(actual, option1) && !bytes.Equal(actual, option2) {
 		t.Errorf("Actual: %#v; Expected: %#v", actual, option1)
+	}
+}
+
+func TestWriteWithSnappyCodec(t *testing.T) {
+	bb := new(bytes.Buffer)
+	func(w io.Writer) {
+		fw, err := NewWriter(
+			BlockSize(2),
+			Compression(CompressionSnappy),
+			WriterSchema(`"int"`),
+			Sync(defaultSync),
+			ToWriter(w))
+		if err != nil {
+			t.Fatalf("Actual: %#v; Expected: %#v", err, nil)
+		}
+		defer fw.Close()
+		fw.Write(int32(13))
+		fw.Write(int32(42))
+		fw.Write(int32(54))
+		fw.Write(int32(99))
+	}(bb)
+
+	// NOTE: because key value pair ordering is indeterminate,
+	// there are two valid possibilities for the encoded map:
+	compressed := "\x04\x10\x02\x04\x1aT\x9d\xf2}\xc9!\x0fǻ\x81\x869\xacH\xa4Ư\xa2\xf1X\x1a\x04\x12\x03\bl\xc6\x01T+\xab\b"
+	option1 := []byte("Obj\x01\x04\x16avro.schema\x0a\x22int\x22\x14avro.codec\x0csnappy\x00" + string(defaultSync) + compressed + string(defaultSync))
+	option2 := []byte("Obj\x01\x04\x14avro.codec\x0csnappy\x16avro.schema\x0a\x22int\x22\x00" + string(defaultSync) + compressed + string(defaultSync))
+
+	actual := bb.Bytes()
+	if !bytes.Equal(actual, option1) && !bytes.Equal(actual, option2) {
+		t.Errorf("Actual: %q; Expected: %q", actual, option1)
 	}
 }
