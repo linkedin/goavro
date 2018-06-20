cfc5228a5abef476a22bbd6b00f61afc7fd50ea7
Kun Devanny <kun.devanny@neulion.com>
2018-06-20 17:07:36 +0100

    Ensure timestamp-millis uses milliseconds and not seconds Initial add of int.date

--- logical_type.go
+++ logical_type.go
@@ -9,6 +9,29 @@ type toNativeFn func([]byte) (interface{}, []byte, error)
 type fromNativeFn func([]byte, interface{}) ([]byte, error)
 
 ///////////////////////////////////////////////
+// date logical type - to/from UTC
+///////////////////////////////////////////////
+func dateToNative(fn toNativeFn) toNativeFn {
+	return func(b []byte) (interface{}, []byte, error) {
+		l, b, err := fn(b)
+		if err != nil {
+			return l, b, err
+		}
+		i, ok := l.(int)
+		if !ok {
+			// This error condition will not trigger with the existing longX functions as they only error on short buffer error
+			return l, b, fmt.Errorf("cannot transform native timestamp-millis, expected int64, received %t", l)
+		}
+		t := time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC).AddDate(0, 0, i).UTC()
+		return t, b, nil
+	}
+}
+
+func dateFromNative(fn fromNativeFn) fromNativeFn {
+	return nil
+}
+
+///////////////////////////////////////////////
 // timestamp-millis logical type - to/from UTC
 ///////////////////////////////////////////////
 func timeStampMillisToNative(fn toNativeFn) toNativeFn {
@@ -19,10 +42,11 @@ func timeStampMillisToNative(fn toNativeFn) toNativeFn {
 		}
 		i, ok := l.(int64)
 		if !ok {
-			// This error condition will not trigger with the existing longX functions as they only error on short buffer error
 			return l, b, fmt.Errorf("cannot transform native timestamp-millis, expected int64, received %t", l)
 		}
-		return time.Unix(i, 0).UTC(), b, nil
+		secs := i / 1e3
+		nanosecs := i - (secs * 1e3)
+		return time.Unix(secs, nanosecs).UTC(), b, nil
 	}
 }
 
@@ -32,6 +56,7 @@ func timeStampMillisFromNative(fn fromNativeFn) fromNativeFn {
 		if !ok {
 			return nil, fmt.Errorf("cannot transform binary timestamp-millis, expected time.Time, received %T", d)
 		}
-		return fn(b, t.Unix())
+		millisecs := t.UnixNano() / 1e6
+		return fn(b, millisecs)
 	}
 }
--- logical_type_test.go
+++ logical_type_test.go
@@ -13,11 +13,11 @@ func TestTimeStampMillisLogicalTypeEncode(t *testing.T) {
 	schema := `{"type": "long", "logicalType": "timestamp-millis"}`
 	testBinaryDecodeFail(t, schema, []byte(""), "short buffer")
 	testBinaryEncodeFail(t, schema, "test", "cannot transform binary timestamp-millis, expected time.Time")
-	testBinaryCodecPass(t, schema, time.Date(2006, 1, 2, 15, 04, 05, 0, time.UTC), []byte("\xca\x83\xca\xbb\x08"))
+	testBinaryCodecPass(t, schema, time.Date(2006, 1, 2, 15, 04, 05, 0, time.UTC), []byte("\x90\xfa\xab\xba\x91\x42"))
 }
 
 func TestTimeStampMillisLogicalTypeUnionEncode(t *testing.T) {
 	schema := `{"type": ["null", {"type": "long", "logicalType": "timestamp-millis"}]}`
 	testBinaryEncodeFail(t, schema, Union("string", "test"), "cannot encode binary union: no member schema types support datum: allowed types: [null timestamp-millis]")
-	testBinaryCodecPass(t, schema, Union("timestamp-millis", time.Date(2006, 1, 2, 15, 04, 05, 0, time.UTC)), []byte("\x02\xca\x83\xca\xbb\x08"))
+	testBinaryCodecPass(t, schema, Union("timestamp-millis", time.Date(2006, 1, 2, 15, 04, 05, 0, time.UTC)), []byte("\x02\x90\xfa\xab\xba\x91\x42"))
 }
