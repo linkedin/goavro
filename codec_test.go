9e3ffbc22b459192a7090bfd06433b24be086370
Karrick S. McDermott <kmcdermott@linkedin.com>
2018-08-22 11:31:26 -0400

    added remainder of rabin fingerprint test cases from Avro repos
    resolve #35

--- codec_test.go
+++ codec_test.go
@@ -63,6 +63,66 @@ func TestSchemaCRC64Avro(t *testing.T) {
 			Schema:      `[ "int" , {"type":"boolean"} ]`,
 			Fingerprint: 5392556393470105090,
 		},
+		{
+			Schema:      `{"fields":[], "type":"record", "name":"foo"}`,
+			Fingerprint: -4824392279771201922,
+		},
+		{
+			Schema:      `{"fields":[], "type":"record", "name":"foo", "namespace":"x.y"}`,
+			Fingerprint: 5916914534497305771,
+		},
+		{
+			Schema:      `{"fields":[], "type":"record", "name":"a.b.foo", "namespace":"x.y"}`,
+			Fingerprint: -4616218487480524110,
+		},
+		{
+			Schema:      `{"fields":[], "type":"record", "name":"foo", "doc":"Useful info"}`,
+			Fingerprint: -4824392279771201922,
+		},
+		{
+			Schema:      `{"fields":[], "type":"record", "name":"foo", "aliases":["foo","bar"]}`,
+			Fingerprint: -4824392279771201922,
+		},
+		{
+			Schema:      `{"fields":[], "type":"record", "name":"foo", "doc":"foo", "aliases":["foo","bar"]}`,
+			Fingerprint: -4824392279771201922,
+		},
+		{
+			Schema:      `{"fields":[{"type":{"type":"boolean"}, "name":"f1"}], "type":"record", "name":"foo"}`,
+			Fingerprint: 7843277075252814651,
+		},
+		{
+			Schema:      `{ "fields":[{"type":"boolean", "aliases":[], "name":"f1", "default":true}, {"order":"descending","name":"f2","doc":"Hello","type":"int"}], "type":"record", "name":"foo"}`,
+			Fingerprint: -4860222112080293046,
+		},
+		{
+			Schema:      `{"type":"enum", "name":"foo", "symbols":["A1"]}`,
+			Fingerprint: -6342190197741309591,
+		},
+		{
+			Schema:      `{"namespace":"x.y.z", "type":"enum", "name":"foo", "doc":"foo bar", "symbols":["A1", "A2"]}`,
+			Fingerprint: -4448647247586288245,
+		},
+		{
+			Schema:      `{"name":"foo","type":"fixed","size":15}`,
+			Fingerprint: 1756455273707447556,
+		},
+		{
+			Schema:      `{"namespace":"x.y.z", "type":"fixed", "name":"foo", "doc":"foo bar", "size":32}`,
+			Fingerprint: -3064184465700546786,
+		},
+		{
+			Schema:      `{ "items":{"type":"null"}, "type":"array"}`,
+			Fingerprint: -589620603366471059,
+		},
+		{
+			Schema:      `{ "values":"string", "type":"map"}`,
+			Fingerprint: -8732877298790414990,
+		},
+		{
+			Schema:      `{"name":"PigValue","type":"record", "fields":[{"name":"value", "type":["null", "int", "long", "PigValue"]}]}`,
+			Fingerprint: -1759257747318642341,
+		},
 	}
 
 	for _, c := range cases {
