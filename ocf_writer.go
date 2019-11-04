8f43608274876f18b4a96f5c84018a67bd40b780
Nick Mykins <nmykins@digitalocean.com>
2019-11-04 11:14:36 -0500

    make it compile lol

--- ocf_writer.go
+++ ocf_writer.go
@@ -161,13 +161,13 @@ func (ocfw *OCFWriter) quickScanToTail(ior io.Reader) error {
 func (ocfw *OCFWriter) Append(data interface{}) (int, error) {
 	arrayValues, err := convertArray(data)
 	if err != nil {
-		return err
+		return 0, err
 	}
 
 	// Chunk data so no block has more than MaxBlockCount items.
 	for int64(len(arrayValues)) > MaxBlockCount {
-		if err := ocfw.appendDataIntoBlock(arrayValues[:MaxBlockCount]); err != nil {
-			return err
+		if n, err := ocfw.appendDataIntoBlock(arrayValues[:MaxBlockCount]); err != nil {
+			return 0, err
 		}
 		arrayValues = arrayValues[MaxBlockCount:]
 	}
@@ -181,7 +181,7 @@ func (ocfw *OCFWriter) appendDataIntoBlock(data []interface{}) (int, error) {
 	// Encode and concatenate each data item into the block
 	for _, datum := range data {
 		if block, err = ocfw.header.codec.BinaryFromNative(block, datum); err != nil {
-			return fmt.Errorf("cannot translate datum to binary: %v; %s", datum, err)
+			return 0, fmt.Errorf("cannot translate datum to binary: %v; %s", datum, err)
 		}
 	}
 
@@ -196,10 +196,10 @@ func (ocfw *OCFWriter) appendDataIntoBlock(data []interface{}) (int, error) {
 		cw, _ := flate.NewWriter(bb, flate.DefaultCompression)
 		// writing bytes to cw will compress bytes and send to bb.
 		if _, err := cw.Write(block); err != nil {
-			return err
+			return 0, err
 		}
 		if err := cw.Close(); err != nil {
-			return err
+			return 0, err
 		}
 		block = bb.Bytes()
 
@@ -213,7 +213,7 @@ func (ocfw *OCFWriter) appendDataIntoBlock(data []interface{}) (int, error) {
 		block = compressed
 
 	default:
-		return fmt.Errorf("should not get here: cannot compress block using unrecognized compression: %d", ocfw.header.compressionID)
+		return 0, fmt.Errorf("should not get here: cannot compress block using unrecognized compression: %d", ocfw.header.compressionID)
 
 	}
 
@@ -224,7 +224,7 @@ func (ocfw *OCFWriter) appendDataIntoBlock(data []interface{}) (int, error) {
 	buf = append(buf, block...)                      // serialized objects
 	buf = append(buf, ocfw.header.syncMarker[:]...)  // sync marker
 
-	n, err = ocfw.iow.Write(buf)
+	n, err := ocfw.iow.Write(buf)
 	return n, err
 }
 
